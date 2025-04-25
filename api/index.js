9e2e84a043f468ab8de7fb830d37c4451f3d44dd
Nikhil Verma <vermanick75@gmail.com>
2025-04-25 22:31:09 +0530

    improved folder structure

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const dataRoutes = require('./routes/dataRoutes');
+const dataRoutes = require('../routes/dataRoutes');
 
 dotenv.config();
 const app = express();
